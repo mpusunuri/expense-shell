03892e000d0adc9bab9e08540844344996583c68
mpusunuri <mpusunuri@gmail.com>
2025-03-08 16:52:11 +0530

    expense-shell automation script added

--- mysql.sh
+++ mysql.sh
@@ -42,5 +42,14 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>> $LOG_FILE_NAME
 VALIDATE $? "Starting MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "Setting Root Password"
+mysql_secure_installation --set-root-pass ExpenseApp@1 -e 'show databases'; &>> $LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    echo "mysql Root Password not set up" &>> $LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting Root Password"
+
+else
+    echo -e "mysql Root Password already set up ... &Y SKIPPING &N"
+fi
